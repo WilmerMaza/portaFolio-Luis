09d6c364dfb3e78ad1830a9efc0495743d60f799
SergioBJ <Serbeju00@gmail.com>
2024-02-29 02:00:44 -0500

    PrimeraEntrega

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SegundaParteComponent } from './segunda-parte/segunda-parte.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [SegundaParteComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
---
+++ src/app/views/home/segunda-parte/segunda-parte.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { HomeComponent } from '../home.component';
+@Component({
+  selector: 'app-segunda-parte',
+  standalone: true,
+  imports: [],
+  templateUrl: './segunda-parte.component.html',
+  styleUrl: './segunda-parte.component.scss'
+})
+export class SegundaParteComponent {
+
+}
