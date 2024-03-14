88b567da2e46d4efd66edf8b7e599997dab9deb7
SergioBJ <Serbeju00@gmail.com>
2024-03-14 00:02:59 -0500

    cuarta-parte3

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { PrincipalBannerComponent } from '../principal-banner/principal-banner.component';
 import { SegundaParteComponent } from '../segunda-parte/segunda-parte.component';
+import { TerceraParteComponent } from '../tercera-parte/tercera-parte.component';
+import { CuartaParteComponent } from '../cuarta-parte/cuarta-parte.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [PrincipalBannerComponent,SegundaParteComponent],
+  imports: [PrincipalBannerComponent,SegundaParteComponent, TerceraParteComponent, CuartaParteComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
